7d6b2ce0b526ee53ed0b7f766232bfead83d1a0a
Kier Davis <kierdavis@gmail.com>
2015-03-10 19:24:43 +0000

    Finish parser

---
+++ decoder.go
@@ -0,0 +1,90 @@
+package ihex
+
+import (
+	"bufio"
+    "fmt"
+	"io"
+    "os"
+    "strings"
+)
+
+// Parses IHEX files.
+type Decoder struct {
+	scanner *bufio.Scanner
+    rec Record
+    err error
+    lineno int
+}
+
+// Create a new Decoder using input from the given io.Reader.
+func NewDecoder(r io.Reader) (d *Decoder) {
+	scanner := bufio.NewScanner(r)
+	return &Decoder{
+		scanner: scanner,
+        lineno: 0,
+	}
+}
+
+// Read and decode one record from the source, returning true if the record was
+// decoded successfully. False may be returned if the end of the file is reached
+// or an error has occurred. This method is designed to be called repeatedly
+// to read the entire file, and then have the Err method called to determine
+// if an error occurred. The scanned record can be retrieved with the Record
+// method.
+func (d *Decoder) Scan() (ok bool) {
+    if d.err != nil || !d.scanner.Scan() {
+        return false
+    }
+    
+    d.lineno++
+    
+    line := strings.Trim(d.scanner.Text(), " \t\r\n")
+    if len(line) == 0 {
+        // try again
+        return d.Scan()
+    }
+    
+    if line[0] != ':' {
+        d.err = fmt.Errorf("parse error at line %d: line does not begin with a colon", d.lineno)
+        return false
+    }
+    
+    rec, err := DecodeRecordHex(line)
+    if err != nil {
+        d.err = fmt.Errorf("parse error at line %d: %s", d.lineno, err.Error())
+        return false
+    } else {
+        d.rec = rec
+        return true
+    }
+    
+}
+
+// Returns the most recently parsed record. An empty record is returned if Scan
+// has not yet been called, or if Scan has never returned true.
+func (d *Decoder) Record() (rec Record) {
+    return d.rec
+}
+
+// Returns the first error that occurred during parsing, or nil if there was no
+// error.
+func (d *Decoder) Err() (err error) {
+    if d.err == nil {
+        return d.scanner.Err()
+    }
+    return d.err
+}
+
+func ExampleDecoder() {
+    d := NewDecoder(os.Stdin)
+    
+    for d.Scan() {
+        // do something with d.Record()
+    }
+    
+    // check if there was an error during parsing
+    err := d.Err()
+    if err != nil {
+        // handle the error
+    }
+}
--- record.go
+++ record.go
@@ -1,10 +1,10 @@
 package ihex
 
 import (
-	"fmt"
+    "encoding/hex"
+    "fmt"
+    "strings"
 )
-
-var ChecksumMismatchError = fmt.Errorf("DecodeRecord: checksum mismatch")
 
 type RecordType uint8
 
@@ -13,12 +13,20 @@ const (
 	EndOfFile RecordType = 0x01
 )
 
+// Represents a single IHEX record.
 type Record struct {
+    // The type of record (either Data or EndOfFile).
+    Type RecordType
+    
+    // The address assocated with the record's data (for Data records).
 	Address uint16
-	Type RecordType
+    
+    // The payload of the record.
 	Data []byte
 }
 
+// Interpreting bytes as the binary form of an IHEX record, decode and return a
+// Record. err may be non-nil if there was a checksum mismatch.
 func DecodeRecord(bytes []byte) (rec Record, err error) {
 	dataLen := int(bytes[0])
 	address := (uint16(bytes[1]) << 8) | uint16(bytes[2])
@@ -26,19 +34,32 @@ func DecodeRecord(bytes []byte) (rec Record, err error) {
 	
 	checksumIndex := 4 + dataLen
 	if bytes[checksumIndex] != Checksum(bytes[:checksumIndex]) {
-		return rec, ChecksumMismatchError
+		return rec, fmt.Errorf("checksum mismatch")
 	}
 	
 	data := make([]byte, dataLen)
 	copy(data, bytes[4:checksumIndex])
 	
 	return Record{
+        Type: recType,
 		Address: address,
-		Type: recType,
 		Data: data,
 	}, nil
 }
 
+// Interpreting hexStr as the hexadecimal form of an IHEX record with or without
+// the leading start token (colon), deocde and return a Record. err may be
+// non-nil if there was a checksum mismatch or if the decoding from hexadecimal
+// failed.
+func DecodeRecordHex(hexStr string) (rec Record, err error) {
+    bytes, err := hex.DecodeString(strings.TrimLeft(hexStr, ":"))
+    if err != nil {
+        return rec, err
+    }
+    return DecodeRecord(bytes)
+}
+
+// Encode the record to the binary form of an IHEX record.
 func (rec Record) Encode() (bytes []byte) {
 	bytes = make([]byte, len(rec.Data) + 5)
 	bytes[0] = byte(len(rec.Data))
@@ -47,7 +68,13 @@ func (rec Record) Encode() (bytes []byte) {
 	bytes[3] = byte(rec.Type)
 	copy(bytes[4:], rec.Data)
 	
-	last := len(bytes) - 1
-	bytes[last] = Checksum(bytes[:last])
+	checksumIndex := 4 + len(rec.Data)
+	bytes[checksumIndex] = Checksum(bytes[:checksumIndex])
 	return bytes
+}
+
+// Encode the record to the hexadecimal form of an IHEX record, with the leading
+// start token (colon).
+func (rec Record) EncodeHex() (hexStr string) {
+    return hex.EncodeToString(rec.Encode())
 }
